e069082725e82a9f3fe566876b30ae8074b6b1c4
jangel-pz <japradazamora@gmail.com>
2026-08-14 12:54:35 +0200

    chore: configuración y dependencias para API de IA

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    // Aumentado limite de MB en peticiones de Server Actions
+    serverActions: {
+      bodySizeLimit: "15mb",
+    },
+  },
 };
 
 export default nextConfig;
